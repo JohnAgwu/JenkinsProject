07c4e37ae14a86be59c8a2851a6d987c11ab288f
John Agwu <agujohnifeanyi69@gmail.com>
2024-11-23 00:58:16 +0000

    changed to port 80 for nginx and added curl to test nginx

--- my-module/sg.tf
+++ my-module/sg.tf
@@ -6,17 +6,20 @@ resource "aws_security_group" "some-rule" {
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
   ingress  {
     from_port = var.ingress_port_1
     to_port = var.ingress_port_1
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
     ingress  {
     from_port = var.ingress_port_2
     to_port = var.ingress_port_2
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
