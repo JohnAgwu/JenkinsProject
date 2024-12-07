924b67091dfadca6e5659858d0e9afbc91e7a688
John Agwu <agujohnifeanyi69@gmail.com>
2024-12-07 18:46:03 +0000

    updated

--- dev/instance.tf
+++ dev/instance.tf
@@ -1,29 +1,29 @@
 module "nginx-machine" {
-  source         = "../my-module"
-  node_name      = var.node1
-  sg_name        = var.node1-sg-name
-  ingress_port_1 = var.node1-port-1 //from 22
-  ingress_port_2 = var.node1-port-2 //to 80
-  ingress_port_3 = var.node1-port-3 //to 8080
+  source             = "../my-module"
+  node_name          = var.node1
+  sg_name            = var.node1-sg-name
+  ingress_port_1     = var.node1-port-1 //from 22
+  ingress_port_2     = var.node1-port-2 //to 80
+  ingress_port_3     = var.node1-port-3 //to 8080
   nginx_private_cidr = "10.0.1.0/24"
 }
 module "python-machine" {
-  source         = "../my-module"
-  node_name      = var.node2
-  sg_name        = var.node2-sg-name
-  ingress_port_1 = var.node2-port-1 //from 22
-  ingress_port_2 = var.node2-port-2 //to 65432
-  ingress_port_3 = var.node1-port-3 //to 8080
+  source             = "../my-module"
+  node_name          = var.node2
+  sg_name            = var.node2-sg-name
+  ingress_port_1     = var.node2-port-1 //from 22
+  ingress_port_2     = var.node2-port-2 //to 65432
+  ingress_port_3     = var.node1-port-3 //to 8080
   nginx_private_cidr = "10.0.1.0/24"
 }
 
 module "python-machine2" {
-  source         = "../my-module"
-  node_name      = var.node3
-  sg_name        = var.node3-sg-name
-  ingress_port_1 = var.node3-port-1 //from 22
-  ingress_port_2 = var.node3-port-2 //to 65432
-  ingress_port_3 = var.node1-port-3 //to 8080
+  source             = "../my-module"
+  node_name          = var.node3
+  sg_name            = var.node3-sg-name
+  ingress_port_1     = var.node3-port-1 //from 22
+  ingress_port_2     = var.node3-port-2 //to 65432
+  ingress_port_3     = var.node1-port-3 //to 8080
   nginx_private_cidr = "10.0.1.0/24"
 }
 
