58a89b44dad95890b8c8a4a1c825b62f610b66df
John Agwu <agujohnifeanyi69@gmail.com>
2024-12-09 21:13:42 +0000

    updated

--- dev/instance.tf
+++ dev/instance.tf
@@ -6,10 +6,10 @@ module "nginx-machine" {
   ingress_port_2       = var.node1-port-2 //to 80
   ingress_port_3       = var.node1-port-3 //to 8080
   ingress_port_4       = var.node1-port-4 //to 443
-  nginx_private_cidr_1 = ["0.0.0.0/0"]
-  nginx_private_cidr_2 = ["0.0.0.0/0"]
-  nginx_private_cidr_3 = ["0.0.0.0/0"]
-  nginx_private_cidr_4 = ["0.0.0.0/0"]
+  nginx_private_cidr_1 = "0.0.0.0/0"
+  nginx_private_cidr_2 = "0.0.0.0/0"
+  nginx_private_cidr_3 = "0.0.0.0/0"
+  nginx_private_cidr_4 = "0.0.0.0/0"
 }
 module "python-machine" {
   source               = "../my-module"
@@ -19,10 +19,10 @@ module "python-machine" {
   ingress_port_2       = var.node2-port-2 //to 65432
   ingress_port_3       = var.node2-port-3 //to 8080
   ingress_port_4       = var.node2-port-4 //to 443
-  nginx_private_cidr_1 = ["10.0.1.0/24"]
-  nginx_private_cidr_2 = ["10.0.1.0/24"]
-  nginx_private_cidr_3 = ["10.0.1.0/24"]
-  nginx_private_cidr_4 = ["10.0.1.0/24"]
+  nginx_private_cidr_1 = "10.0.1.0/24"
+  nginx_private_cidr_2 = "10.0.1.0/24"
+  nginx_private_cidr_3 = "10.0.1.0/24"
+  nginx_private_cidr_4 = "10.0.1.0/24"
 }
 
 module "python-machine2" {
@@ -33,10 +33,10 @@ module "python-machine2" {
   ingress_port_2       = var.node3-port-2 //to 65432
   ingress_port_3       = var.node3-port-3 //to 8080
   ingress_port_4       = var.node3-port-4 //to 443
-  nginx_private_cidr_1 = ["10.0.1.0/24"]
-  nginx_private_cidr_2 = ["10.0.1.0/24"]
-  nginx_private_cidr_3 = ["10.0.1.0/24"]
-  nginx_private_cidr_4 = ["10.0.1.0/24"]
+  nginx_private_cidr_1 = "10.0.1.0/24"
+  nginx_private_cidr_2 = "10.0.1.0/24"
+  nginx_private_cidr_3 = "10.0.1.0/24"
+  nginx_private_cidr_4 = "10.0.1.0/24"
 }
 
 output "nginx_machine_public_dns" {
