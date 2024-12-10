1c8b7073fbf8d45dbb523e0dda1b9978a26d9f5b
John Agwu <agujohnifeanyi69@gmail.com>
2024-12-10 00:49:04 +0000

    updated

--- dev/instance.tf
+++ dev/instance.tf
@@ -2,7 +2,7 @@ module "nginx-machine" {
   source               = "../my-module"
   node_name            = var.node1
   sg_name              = var.node1-sg-name
-  ingress_port_1       = var.node1-port-1 //from 22
+  ingress_port_1       = var.node1-port-1 //to 22
   ingress_port_2       = var.node1-port-2 //to 80
   ingress_port_3       = var.node1-port-3 //to 8080
   ingress_port_4       = var.node1-port-4 //to 443
@@ -15,7 +15,7 @@ module "python-machine" {
   source               = "../my-module"
   node_name            = var.node2
   sg_name              = var.node2-sg-name
-  ingress_port_1       = var.node2-port-1 //from 22
+  ingress_port_1       = var.node2-port-1 //to 22
   ingress_port_2       = var.node2-port-2 //to 65432
   ingress_port_3       = var.node2-port-3 //to 8080
   ingress_port_4       = var.node2-port-4 //to 443
@@ -29,7 +29,7 @@ module "python-machine2" {
   source               = "../my-module"
   node_name            = var.node3
   sg_name              = var.node3-sg-name
-  ingress_port_1       = var.node3-port-1 //from 22
+  ingress_port_1       = var.node3-port-1 //to 22
   ingress_port_2       = var.node3-port-2 //to 65432
   ingress_port_3       = var.node3-port-3 //to 8080
   ingress_port_4       = var.node3-port-4 //to 443
